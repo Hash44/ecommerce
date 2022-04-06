a0f69dd91bbaf2036bc661ea6b1770547d5fa23d
Hashim <hashimdev1@gmail.com>
2022-04-07 02:51:36 +0300

    Create simple front page (latest products)

--- vuecommerce/babel.config.js
+++ vuecommerce/babel.config.js
@@ -3,3 +3,20 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+
+
+// module.exports = {
+//   presets: [
+//     ['@vue/app', {
+//       polyfills: [
+//         'es.promise',
+//         'es.symbol',
+
+//       ]
+//     }]
+//   ]
+// }
+
+// If you want to include a polyfill, you need to:
+//         - add a fallback 'resolve.fallback: { "zlib": require.resolve("browserify-zlib") }'
+//         - install 'browserify-zlib'
