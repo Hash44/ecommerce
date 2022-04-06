67f858561c5bd339bf06e3a06884e53cd14a7af6
Hashim <hashimdev1@gmail.com>
2022-04-06 13:23:13 +0300

    Create serializer and views for the products

--- dcommerce/dcommerce/urls.py
+++ dcommerce/dcommerce/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
+from xml.dom.minidom import Document
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,5 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/', include('product.urls')),
+
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
