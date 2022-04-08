f89b62fb191e765b4c74f82d67711a6692e476f0
Hashim <hashimdev1@gmail.com>
2022-04-08 05:05:38 +0300

    add ProductBox components

--- vuecommerce/src/views/HomeView.vue
+++ vuecommerce/src/views/HomeView.vue
@@ -14,30 +14,17 @@
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
-      <div
-        class="column is-3"
+      <ProductBox 
         v-for="product in latestProducts"
         v-bind:key="product.id"
-        >
-        <div class="box">
-          <figure class="image mb-4">
-            <img v-bind:src="product.get_thumbnail" src="" alt="">
-          </figure>
-          <h3 class="is-size-4">{{ product.name}}</h3>
-          <p class="is-size-6 has-text-grey">{{ product.price }}</p>
-          <router-link
-            v-bind:to="product.get_absolute_url"
-            class="button is-dark mt-4">
-            View Details
-          </router-link>
-        </div>
-      </div>
+        v-bind:product="product" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ProductBox from '@/components/ProductBox.vue'
 
 export default {
   name: 'Home',
@@ -46,14 +33,18 @@ export default {
       latestProducts: []
     }
   },
-  components: {
+  components: { 
+    ProductBox
   },
   mounted() {
     this.getLatestProducts()
+
+    document.title =' Home | Dcommerce'
   },
   methods: {
-    getLatestProducts() {
-      axios
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+      await axios
         .get('/api/v1/latest-products/')
         .then(response => {
           this.latestProducts = response.data
@@ -61,15 +52,8 @@ export default {
         .catch(error => {
           console.log(error)
         })
+      this.$store.commit('setIsLoading', false)
     }
   }
 }
 </script>
-
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
